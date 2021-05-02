b220819c26bab16c83d66c1de415970a0903ba22
tkm1988 <bumpbumpbump.v@gmail.com>
2021-05-02 09:41:25 +0900

    update

--- .emacs.el
+++ .emacs.el
@@ -133,13 +133,23 @@
 ;;          lsp-mode
 ;;============================
 ;; Make         : 2021/04/30
-;; Last Modifiy : 2021/04/30
+;; Last Modifiy : 2021/05/02
 ;; Note         : -
 
 (straight-use-package 'lsp-mode)
 (straight-use-package 'lsp-ivy)
-(use-package lsp-mode)
-(use-package lsp-ivy)
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (web-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 
 
 ;;============================
